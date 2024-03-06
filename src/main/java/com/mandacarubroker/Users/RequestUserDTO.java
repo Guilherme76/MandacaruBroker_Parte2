2003f4457b4c36cc04c91e07335639fd1f1cb2d5
Emanuel <emanuelcicero40@gmail.com>
2024-03-06 08:21:00 -0300

    Att Pom e RequestUserDTO

--- src/main/java/com/mandacarubroker/Users/RequestUserDTO.java
+++ src/main/java/com/mandacarubroker/Users/RequestUserDTO.java
@@ -1,6 +1,7 @@
 package com.mandacarubroker.Users;
 
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import org.checkerframework.common.aliasing.qual.Unique;
@@ -9,20 +10,22 @@ public record RequestUserDTO(
     @Unique
     @Pattern(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}",
             message = "Endereço de e-mail inválido ou em branco")
-    @NotNull
+    @NotBlank
     String email,
-    @NotNull
+    @NotBlank
     @Pattern(regexp = "^((19|20)\\d\\d)-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])$",
             message = "Formato de data inválida")
     String birthDate,
     @Unique
-    @NotNull
+    @NotBlank
     String username,
-    @NotNull
+    @NotBlank
     String password,
+    @NotBlank
     String firstName,
+    @NotBlank
     String lastName,
-    @NotNull
+    @NotBlank
     double balance
 ) {
 }
