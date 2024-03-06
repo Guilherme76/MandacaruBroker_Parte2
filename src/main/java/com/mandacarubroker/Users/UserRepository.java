50bfc7891eee96364a1c919d4c425968c759df7d
Guilherme <gilherme.vasconvelos@gmail.com>
2024-03-06 05:31:39 -0300

    adicionado validação de entrada de usuario

--- src/main/java/com/mandacarubroker/Users/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mandacarubroker.Users;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface UserRepository  extends JpaRepository<Users, String> {
-}
